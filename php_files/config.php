39d42e9c93e53c818816610958eaf1400137112d
mahmoodsd <126504808+mahmoodsd@users.noreply.github.com>
2023-04-13 10:23:49 +0300

    i add the first file of php this file to connection

---
+++ php_files/config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost:3306";  //my computer
+$username = "root";
+$password = "1234";
+$db =  "freelancer";
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+?>
