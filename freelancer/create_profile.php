5bf8031b397d5fdbf3dbc85ec4a6d8d39945252a
mahmoodsd <126504808+mahmoodsd@users.noreply.github.com>
2023-05-07 13:43:25 +0300

    complete freelancer Scenario , signup to profile

--- freelancer/create_profile.php
+++ freelancer/create_profile.php
@@ -6,40 +6,141 @@ include('config.php');
 
 $conn = mysqli_connect($servername, $username, $password, $db);
 
+$freelancer_id = $_GET['id'];
 
 
- // POST the form data
-
-$namee = $_POST['namee'];
-$age = $_POST['age'];
-$phone = $_POST['phone'];
-$country = $_POST['country'];
-$city = $_POST['city'];
-$addresss = $_POST['addresss'];
-$purview = $_POST['purview'];
-$skills = $_POST['skills'];
-$otherskills = $_POST['otherskills'];
-$languagee = $_POST['languagee'];
-$otherlanguage = $_POST['otherlanguagee'];
-$experiences = $_POST['experiences'];
-$rate = $_POST['rate'];
-$bio = $_POST['bio'];
-$zip = $_POST['zip'];
-
-
-
-//sql query
-$sql = "INSERT INTO profile_tb (namee,  purview  ,age  , country  ,city ,  addresss,   zip,  languagee   , otherlanguage, skills,otherskills   ,experiences  ,rate  ,bio ,phone)
-                        VALUES ('$namee', '$purview' ,'$age' , '$country' ,'$city' , '$addresss',  '$zip', '$languagee' ,'$otherlanguage','$skills','$otherskills'  ,'$experiences' ,'$rate' ,'$bio' ,'$phone');";
-
-//check sql process
-if (mysqli_query($conn, $sql)) {
-
-  header("location: create_profile.html");
-  exit;
-
+if(isset($_POST['button'])) {
+  extract($_POST);
+        
+  $sql = mysqli_query($conn, "INSERT INTO profile_tb (namee, purview, age, country, city, addresss, zip, languagee, otherlanguage, skills, otherskills, experiences, rate, bio, phone ,freelancer_id)
+                              VALUES ('$namee', '$purview', '$age', '$country', '$city', '$addresss', '$zip', '$languagee', '$otherlanguage', '$skills', '$otherskills', '$experiences', '$rate', '$bio', '$phone' , '$freelancer_id')");
+                              
+  if($sql) {           
+    $id = mysqli_insert_id($conn); // get the ID of the newly inserted row
+    header("refresh:2;url=profile.php?id=$id"); // redirect the user to the profile page
+  } else {
+    $message = "Error: " . mysqli_error($conn); // display the error message
+  }
 } else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  $message = "Please complete all fields!";
 }
 
 ?>
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create Profile | Freelancer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="create_profile.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
+	<header>
+		<div class="container1">
+			<a href="#" class="logo">Smart</a>
+			<nav>
+				<ul>
+					<li><a href="#">Find Work</a></li>
+					<li><a href="HOME.php">Home</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<section class="hero">
+		<div class="container1">
+			<h1>Create Your Freelancer Profile</h1>
+		</div>
+	</section>
+
+	<section class="form">
+		<div class="container">
+			<form  action="" method="post">
+				<label >Name</label>
+				<input type="text"  name="namee" required>
+
+				<label >Age</label>
+				<input type="number" name="age" required>
+
+				<label >Phone</label>
+				<input type="tel"  name="phone" required>
+
+				<label >Country</label>
+				<input type="text"  name="country" required>
+
+				<label >City</label>
+				<input type="text"  name="city" required>
+
+				<label >Address</label>
+				<input type="text"  name="addresss" required>
+
+				<label >Zip code</label>
+				<input type="text"  name="zip" required>
+
+				<label >Purview</label>
+				<input type="text" name="purview" required>
+
+				
+				<label >your skills</label>
+				<select  name="skills" required>
+				
+			    <option >IT</option>
+				<option >Programming</option>
+				<option >Front_end</option>
+				<option >DBA</option>
+				<option >Penetration testing</option>
+				<option >Back_end</option>
+				<option >Scrum master</option>
+				<option >designer</option>
+							    </select>
+
+				<label >Other Skills</label>
+				<input type="text"  name="otherskills" required>
+
+				<label >Lnguage</label>
+				<select  name="languagee" required>
+				
+			    <option >English</option>
+				<option >Arabic</option>
+				<option >French</option>
+				<option >Spanish</option>
+				<option >Indian</option>
+				<option >Russian</option>
+				<option >Portuguese</option>
+				<option >Latin language</option>
+							    </select>
+
+				<label >Other Lnguage</label>
+				<input type="text" name="otherlanguage" required>
+
+
+				<label >Experiences (y)</label>
+				<input type="number"  name="experiences" required>
+
+				<label >Hourly Rate ($)</label>
+				<input type="number" name="rate" min="0" step="1" required>
+
+				<label >Bio</label>
+				<textarea id="bio" name="bio"></textarea>
+
+				<button id="button" class="last" type="submit" name="button">Create Profile</button>
+
+			</form>
+		</div>
+	</section>
+
+
+</body>
+</html>
--- freelancer/freelancer.php
+++ freelancer/freelancer.php
@@ -8,33 +8,136 @@ $conn = mysqli_connect($servername, $username, $password, $db);
 
 
 
- // POST the form data
+//  // POST the form data
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$country = $_POST['country'];
-
-
+// $firstname = $_POST['firstname'];
+// $lastname = $_POST['lastname'];
+// $email = $_POST['email'];
+// $password = $_POST['password'];
+// $country = $_POST['country'];
 
 
-//sql query
-$sql = "INSERT INTO freelancer_tb ( F_first_name , F_last_name , F_email, F_password , F_country)
-         VALUES ( '$firstname' , '$lastname' , '$email' , '$password' , '$country');";
 
-$sql .= "INSERT INTO login_tb (email, passwordd)
-        SELECT F_email , F_password FROM freelancer_tb
-        WHERE F_email = '$email' AND F_password = '$password'; ";
 
-//check sql process
-if (mysqli_multi_query($conn, $sql)) {
-  
- header("location: FREELANCER.HTML");
- exit;
+// //sql query
+// $sql = "INSERT INTO freelancer_tb ( F_first_name , F_last_name , F_email, F_password , F_country)
+//          VALUES ( '$firstname' , '$lastname' , '$email' , '$password' , '$country');";
 
+// $sql .= "INSERT INTO login_tb (email, passwordd)
+//         SELECT F_email , F_password FROM freelancer_tb
+//         WHERE F_email = '$email' AND F_password = '$password'; ";
+
+// //check sql process
+// if (mysqli_multi_query($conn, $sql)) {
+
+//   echo "<script>alert('add information successful')</script>";
+//  header("refresh:2;url = create_profile.php?id=$id");
+//  exit;
+
+// } else {
+//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+// }
+
+
+if(isset($_POST['button'])) {
+  extract($_POST);
+        
+  $sql = mysqli_query($conn, "INSERT INTO freelancer_tb ( F_first_name , F_last_name , F_email, F_password , F_country)
+     VALUES ( '$firstname' , '$lastname' , '$email' , '$password' , '$country');");
+                              
+  if($sql) {           
+    $id = mysqli_insert_id($conn); // get the ID of the newly inserted row
+    header("refresh:2;url= create_profile.php?id=$id"); // redirect the user to the profile page
+  } else {
+    $message = "Error: " . mysqli_error($conn); // display the error message
+  }
 } else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  $message = "Please complete all fields!";
 }
 
+
 ?>
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up | Freelancer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="FREELANCER.CSS">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<div class="container1">
+			<a href="#" class="logo">Smart</a>
+			<nav>
+				<ul>
+					<li><a href="#">Find Work</a></li>
+					<li><a href="HOME.php">Home</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Contact Us</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<section class="hero">
+		<div class="container1">
+			<h1>Sign as Freelancer</h1>
+		</div>
+	</section>
+
+	<section class="form">
+		<div class="container">
+			<form  action="" method="post">
+
+
+				<label >Firstname</label>
+				<input type="text"  name="firstname" required>
+
+                <label >Lastname</label>
+				<input type="text"  name="lastname" required>
+		
+
+				<label >Email</label>
+				<input type="email" name="email" required>
+
+
+				<label >Password</label>
+				<input type="password" name="password" required>
+
+
+                <label >Country</label>
+				<select  name="country" required>
+				
+			    <option >Iraq</option>
+				<option >Iran</option>
+				<option >America</option>
+				<option >French</option>
+				<option >Spanish</option>
+				<option >India</option>
+				<option >Russia</option>
+				<option >Italy</option>
+							    
+                </select>
+
+				<button id="button" class="last" type="submit" name="button">Sign UP</button>
+				
+
+			</form>
+		</div>
+	</section>
+
+	
+
+</body>
+</html>
